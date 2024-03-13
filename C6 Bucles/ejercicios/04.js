fc453f878fc26e5ac70fde7cff37382dd0bcad3a
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-12 21:56:40 -0500

    C6 X4 PASSC

--- C6 Bucles/ejercicios/04.js
+++ C6 Bucles/ejercicios/04.js
@@ -3,6 +3,13 @@ function mayoriaDeEdad(edad) {
   // Si tiene 18 años o más, retorna el string: "Allowed".
   // Caso contrario: "Not allowed".
   // Tu código:
+  if (edad>=18) {
+    console.log("Allowed")// Let's have fun
+    return "Allowed"
+  } else {
+    console.log("Not allowed")// You are not allowed
+    return "Not allowed";
+  }
 }
-
+mayoriaDeEdad(19);
 module.exports = mayoriaDeEdad;
