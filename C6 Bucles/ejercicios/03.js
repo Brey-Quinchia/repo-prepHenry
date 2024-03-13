c20d03d2b43395f672b6590fa4ce938b23aea903
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-12 21:51:01 -0500

    C6 EX3 PASS

--- C6 Bucles/ejercicios/03.js
+++ C6 Bucles/ejercicios/03.js
@@ -3,6 +3,16 @@ function obtenerMayor(x, y) {
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
   // Tu código:
+  if (x>y) {
+    console.log("x is greater", x);
+    return x;
+  } else if(x===y){
+    console.log("Both are equal", x, y);
+    return x||y;
+  }else {
+    console.log("Y is greater", y);
+    return y;
+  }
 }
-
+obtenerMayor(3,3);
 module.exports = obtenerMayor;
