6676960892f7a8137f42ad2e0d1bacb190e1bc5f
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-12 22:09:14 -0500

    PASS - C6 X07 is fine

--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,13 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  if (num>=100 && num<=999){
+    console.log("The number have three digits");
+    return true;
+  }else{
+    console.log("The number has not three digits")
+    return false;
+  }
 }
-
+tieneTresDigitos(500);
 module.exports = tieneTresDigitos;
