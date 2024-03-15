affda2b57636cc451b0d7bb09d6b1b8036753d32
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-14 22:43:18 -0500

    C6 x19 & 18 Pass

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,25 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
-}
+  let producto = 1;
 
+  // Si a es mayor que b, intercambiamos los valores
+  if (a > b) {
+    [a, b] = [b, a];
+  }
+
+  // Recorremos los números desde a hasta b (inclusive)
+  for (let i = a; i <= b; i++) {
+    // Multiplicamos el producto actual por el número actual
+    producto *= i;
+    if(producto === -0){
+      producto = 0;
+    }
+  }
+
+  // Devolvemos el producto final
+  console.log(producto);
+  return producto;
+}
+productoEntreNúmeros(-5,0);
 module.exports = productoEntreNúmeros;
--- C6 Bucles/ejercicios/19.js
+++ C6 Bucles/ejercicios/19.js
@@ -2,6 +2,25 @@ function sumarHastaN(n) {
   // La función recibe un número "n" por argumento.
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Tu código:
+  let isNegative;
+  if(n<0){
+    // Validamos si n es negativo
+    n *=-1;
+    isNegative = true;
+  }
+  console.log(n);
+  sum = 0 ; 
+  for (let i = 1; i<=n; i++) {
+    sum +=i;
+    console.log(sum);
+  }
+  if(isNegative){
+    // Al ser negativo, al sumar 1 lo restariamos
+    sum-=1;
+    sum*=-1;
+  }
+  console.log(`The values is ${sum}`);
+  return sum;
 }
-
+sumarHastaN(5);
 module.exports = sumarHastaN;
