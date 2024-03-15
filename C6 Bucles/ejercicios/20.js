db000aac76a2ec850cc80f596a32f81bf141992f
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-14 22:49:38 -0500

    C6 X20 Pass

--- C6 Bucles/ejercicios/20.js
+++ C6 Bucles/ejercicios/20.js
@@ -3,6 +3,15 @@ function sumarHastaNConBreak(n) {
    // Devuelve la suma de todos los números desde 1 hasta n.
    // Si la suma supera a 100, detén el bucle usando break.
    // Tu código:
+   sum = 0;
+   for (let i = 1; i <=n; i++) {
+      sum+=i;
+      if(sum>=100){
+         break;
+   }
+   }
+   console.log(sum);
+   return sum;
 }
-
+sumarHastaNConBreak(15);
 module.exports = sumarHastaNConBreak;
