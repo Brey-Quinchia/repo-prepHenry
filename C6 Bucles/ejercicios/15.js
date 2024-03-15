ec3e1f1597f5b16b1e918f7c505257009fcb787a
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-14 19:30:23 -0500

    c5 X15 Pass, Is too easy

--- C6 Bucles/ejercicios/15.js
+++ C6 Bucles/ejercicios/15.js
@@ -6,6 +6,32 @@ function obtenerDiaSemana(numero) {
   // Si el número no corresponde a un día de la semana, retorna
   // el string "No es un dia de la semana"
   // Tu código:
+  switch (numero) {
+    case 1:
+      console.log("The day is monday");
+      return "Lunes";
+    case 2: 
+      console.log("The day is Tuesday");
+      return "Martes";
+    case 3:
+      console.log("The day is Wednesday");
+      return "Miercoles";
+    case 4:
+      console.log("The day is Thursday");
+      return "Jueves"; 
+    case 5: 
+      console.log("The day is Friday");
+      return "Viernes"; 
+    case 6:
+      console.log("The day is Saturday");
+      return "Sabado";
+    case 7:
+      console.log("The day is Sunday");
+      return "Domingo";
+    default:
+      console.log("No es un dia de la semana");
+      return "No es un dia de la semana";
+  }
 }
-
+obtenerDiaSemana(2); 
 module.exports = obtenerDiaSemana;
