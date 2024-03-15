945c19ba6f38dcdfe573706de2641664f7d3a058
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-14 19:46:41 -0500

    C6 X16 Pass, look good

--- C6 Bucles/ejercicios/16.js
+++ C6 Bucles/ejercicios/16.js
@@ -5,6 +5,22 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!".
   // Si "idioma" no es ninguno de los anteriores o es `undefined`, devuelve "Hola!".
   // Tu código:
+  switch (idioma) {
+    case "aleman":
+      console.log("Guten Tag!"); 
+      return "Guten Tag!"; 
+    case "mandarin":
+      console.log("Ni Hao!"); 
+      return "Ni Hao!"; 
+    case "ingles":
+    console.log("Hello!"); 
+    return "Hello!"; 
+    default:
+      console.log("Hola!"); 
+      return "Hola!"; 
+  }
+
 }
+saludo("aleman");
 
 module.exports = saludo;
