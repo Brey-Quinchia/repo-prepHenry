6173e84645369090eb9ce01d33a42285b75ce60e
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-14 19:59:45 -0500

    C6 x17 Pass

--- C6 Bucles/ejercicios/17.js
+++ C6 Bucles/ejercicios/17.js
@@ -7,6 +7,23 @@ function colors(color) {
   // Si no es ninguno de esos colores           --> "Color not found".
   // PISTA: utilizar el statement SWITCH.
   // Tu código:
+  switch (color) {
+    case "blue":
+      console.log(`This is ${color}`);
+      return `This is blue`;
+    case "red":
+      console.log(`This is ${color}`);
+      return `This is red`;
+    case "green":
+      console.log(`This is ${color}`);
+      return `This is green`;
+    case "orange":
+      console.log(`This is ${color}`);
+      return `This is orange`;
+    default:
+      console.log("Color not found");
+      return "Color not found"; 
+  }
 }
-
+colors("or")
 module.exports = colors;
