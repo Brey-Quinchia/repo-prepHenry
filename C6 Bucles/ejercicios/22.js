b83233135d4c0e243b49c34a7e67f97b6b5b5441
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-17 00:59:24 -0500

    C6 X22 pass - Very difficult

--- C6 Bucles/ejercicios/22.js
+++ C6 Bucles/ejercicios/22.js
@@ -6,6 +6,25 @@ function combine(str1, str2, str3) {
    // combine("abc", "", "123") == "a1b2c3"
    // combine("abc", "12345", "") == "a1b2c345"
    // combine("abc", "12345", "67") == "a16b27c345"
+   largeStr = ""; 
+   for (const string of [str1,str2,str3]) {
+      if (string.length > largeStr.length) {
+         largeStr = string; 
+      }
+   }
+   buildStr = "";
+   for (let counter = 0; counter < largeStr.length; counter++) {
+      charStr1 = Boolean(str1[counter]) ? str1[counter] : ""; 
+      charStr2 = Boolean(str2[counter]) ? str2[counter] : ""; 
+      charStr3 = Boolean(str3[counter]) ? str3[counter] : ""; 
+      //? el metodo boolena retorna false si el string esta vacio 
+      //? O si el string no tiene mas caracteres
+      buildStr += charStr1+charStr2+charStr3; 
+      console.log(buildStr); 
+   }
+   console.log(`The largest str is ${largeStr}`);
+   console.log(`Build str is ${buildStr}`);
+   return buildStr;
 }
-
+combine("abc","12345","67");
 module.exports = combine;
