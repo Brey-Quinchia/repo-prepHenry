d9103fd8676d006017c94bad4ca56948272f966b
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-17 01:12:32 -0500

    C6 X25 Finish - C6 Finish

--- C6 Bucles/ejercicios/25.js
+++ C6 Bucles/ejercicios/25.js
@@ -5,6 +5,20 @@ function esPalindromo(string) {
    // IMPORTANTE: Un palíndromo es una palabra o frase
    // que se lee igual hacia adelante que hacia atrás.
    // Tu código:
+   pureString = string.toLowerCase(); 
+   console.log(pureString);
+   toReverse = Array.from(pureString); 
+   console.log(toReverse);
+   reverStr = toReverse.reverse().join("");//Invertimos el array y luego lo unimos con join
+   console.log(reverStr);
+   if(string === reverStr){
+      console.log(`Word ${string} is same to ${reverStr}`);
+      return true; 
+   }else{
+      console.log(`Word -${pureString}- is not same to -${reverStr}-`);
+      return false; 
+   }
+   
 }
-
+esPalindromo("Pato"); 
 module.exports = esPalindromo;
