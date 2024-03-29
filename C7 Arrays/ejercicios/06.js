39d4dede17472546ac739700cfa11edd8fcb247a
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-28 22:19:59 -0500

    C7 x5 is ok

--- C7 Arrays/ejercicios/06.js
+++ C7 Arrays/ejercicios/06.js
@@ -1,6 +1,10 @@
 function invertirArray(array) {
   // Invierte el arreglo array recibido por argumento.
   // Tu código:
+  array = array.reverse(); 
+  console.log(array);
+  return array; 
 }
-
+invertirArray([1,2,3,4]);
 module.exports = invertirArray;
+// test ok 
