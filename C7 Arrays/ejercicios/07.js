68e9f8135bc317b1d91ec2fbf78780ffc0be8ba5
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-28 22:32:15 -0500

    C7 x8 is ok

--- C7 Arrays/ejercicios/07.js
+++ C7 Arrays/ejercicios/07.js
@@ -2,6 +2,9 @@ function ordenarArray(array) {
   // Ordena los elementos del areglo array de menor a mayor.
   // Devuelve el arreglo resultante.
   // Tu código:
+  array = array.sort(); 
+  console.log(array);
+  return array ;
 }
-
+ordenarArray(["Abc","Bca","Acb"])
 module.exports = ordenarArray;
