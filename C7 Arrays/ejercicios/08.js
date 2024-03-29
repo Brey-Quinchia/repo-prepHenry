fb4e7b8f10b3a3f30e7a63d4d56576807e9eab26
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-29 08:40:13 -0500

    C7 x8 is ok

--- C7 Arrays/ejercicios/08.js
+++ C7 Arrays/ejercicios/08.js
@@ -3,6 +3,22 @@ function encontrarElemento(num, array) {
    // Si lo encuentras debes retornar el INDICE en el que se encuentra dentro del array.
    // Si no se encuentra, retorna -1.
    // Tu código:
-}
+   index = 0
+   equal = false;
+   for (const value of array) {
+      console.log(`Is on value ${value}`);
+      if (value===num) {
+         console.log(`${num} is same to ${value}, Position ->${index}`);
+         equal = true; 
+         return index; 
+      }
+      index++; 
+   }
+   if (equal ===false) {
+      console.log(`There are not matches`);
+      return -1; 
+   }
 
+}
+encontrarElemento(6,[2,3,4,5]);
 module.exports = encontrarElemento;
