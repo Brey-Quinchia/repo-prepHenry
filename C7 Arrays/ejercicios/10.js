78b3bf5e0eced98d34370d3e1e549f306d4c1278
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-29 09:14:35 -0500

    C7 x11 is ok

--- C7 Arrays/ejercicios/10.js
+++ C7 Arrays/ejercicios/10.js
@@ -1,6 +1,16 @@
 function obtenerPrimerStringLargo(array) {
   // Devuelve el primer string con más de 5 caracteres en el array.
   // Tu código:
+  
+  for (let x = 0; x < array.length; x++) {
+    const element = array[x];
+    if (element.length >=5) {
+      console.log(`The element ${element} has more than 5 characters`);
+      return element; 
+    }
+  }
+  console.log(undefined);
+  return undefined;
 }
-
+obtenerPrimerStringLargo(["he","wrld",2,"ANTY",3,4,"HOL"])
 module.exports = obtenerPrimerStringLargo;
--- C7 Arrays/ejercicios/11.js
+++ C7 Arrays/ejercicios/11.js
@@ -2,6 +2,13 @@ function duplicarElementos(array) {
   // Duplica (multiplica x2) cada elemento del array de números.
   // Devuelve un array con los duplicados.
   // Tu código:
+  dupArray = []; 
+  for (const iterator of array) {
+    dupArray.push(iterator*2); 
+    
+  }
+  console.log(`Array doblado es ->${dupArray}`);
+  return dupArray;
 }
-
+duplicarElementos([1,2,3,4,5])
 module.exports = duplicarElementos;
