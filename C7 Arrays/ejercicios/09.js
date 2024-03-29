09cf77c83bb2f6813fb2757b089525bb79a043a1
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-29 08:50:18 -0500

    C7 x9 is ok

--- C7 Arrays/ejercicios/09.js
+++ C7 Arrays/ejercicios/09.js
@@ -2,6 +2,10 @@ function obtenerElementoAleatorio(array) {
    // Devuelve un elemento aleatorio del arreglo array.
    // PISTA: Usa el método Math.random().
    // Tu código:
+   index = array.length; 
+   rand = Math.floor(index*Math.random()); 
+   console.log(`In position ${rand} is -> ${array[rand]}`);
+   return array[rand]; 
 }
-
+obtenerElementoAleatorio([1,2,3])
 module.exports = obtenerElementoAleatorio;
