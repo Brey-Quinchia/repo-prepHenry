7b6e32bbe6290b24552793ef13407a062c8ce755
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-30 15:37:13 -0500

    C7 x13 is ok

--- C7 Arrays/ejercicios/12.js
+++ C7 Arrays/ejercicios/12.js
@@ -2,6 +2,14 @@ function convertirStringAMayusculas(array) {
   // Convierte a mayúsculas todos los strings del array.
   // Retorna el arreglo resultante.
   // Tu código:
+  upperArray = [];
+  for (const iterator of array) {
+    upperIterator = iterator.toUpperCase(); // convertimos a mayuscula el elemento
+    upperArray.push(upperIterator); // Lo añadimos al final del array
+    
+  }
+  console.log(upperArray);
+  return upperArray;
 }
-
+convertirStringAMayusculas(["hello"]); 
 module.exports = convertirStringAMayusculas;
--- C7 Arrays/ejercicios/13.js
+++ C7 Arrays/ejercicios/13.js
@@ -1,6 +1,14 @@
 function filtrarNumerosPares(array) {
   // Devuelve un arreglo solo con los números pares presentes en el array.
   // Tu código:
+  getEven = [];
+  for (const iterator of array) {
+    if (iterator%2===0) {
+      getEven.push(iterator);
+    }
+  }
+  console.log(`The even number in ${array} are: ${getEven}`);
+  return getEven; 
 }
-
+filtrarNumerosPares([1,2,3,4,5,6])
 module.exports = filtrarNumerosPares;
