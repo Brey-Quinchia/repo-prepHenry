3b849cdd27b40c74635b688002ea90bb4c59f9ce
Brey-Quinchia <brehinerqr27@gmail.com>
2024-03-31 21:15:03 -0500

    C8 X9 Ok

--- C8 Objects + Callbacks/objects/ejercicios/09.js
+++ C8 Objects + Callbacks/objects/ejercicios/09.js
@@ -3,6 +3,13 @@ function nuevoUsuario(nombre, email, password) {
   // Este debe tener las propiedades: "nombre", "email" y "password" con sus respectivos valores.
   // Retorna el objeto creado.
   // Tu código:
+  let User = {
+    nombre: nombre,
+    email: email, 
+    password: password
+  }; 
+  console.log(`The user ${User.nombre} has email ${User.email}`);
+  return User;
 }
-
+nuevoUsuario("Brey","brey404.dev@gmail.com","WdsaZxclPP"); 
 module.exports = nuevoUsuario;
