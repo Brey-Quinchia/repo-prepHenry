a304ecf5a6aa90dff1da3233e1a245c5fbe1106b
Brey-Quinchia <brehinerqr27@gmail.com>
2024-04-01 20:12:32 -0500

    C8 T15 is Ok

--- C8 Objects + Callbacks/objects/ejercicios/15.js
+++ C8 Objects + Callbacks/objects/ejercicios/15.js
@@ -2,6 +2,14 @@ function tienePropiedad(objeto, propiedad) {
   // Verifica si el objeto recibido posee una propiedad con el mismo nombre que el parámetro "propiedad".
   // En ese caso, retorna true. Caso contrario, false.
   // Tu código:
-}
+  if (objeto.hasOwnProperty(propiedad)) {
+    console.log(`El objeto contiene la propiedad ->${propiedad}`);
+    return true; 
+  }else{
+    console.log(`El objeto no tiene la propiedad ${propiedad}`);
+    return false;
+  }
 
+}
+tienePropiedad({name: "Brey"},'name')
 module.exports = tienePropiedad;
