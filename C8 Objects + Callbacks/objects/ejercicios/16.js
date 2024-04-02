e3006ce24162be2d78c275acd3f0e9408d34c39d
Brey-Quinchia <brehinerqr27@gmail.com>
2024-04-01 20:17:39 -0500

    C8 T16 is Ok

--- C8 Objects + Callbacks/objects/ejercicios/16.js
+++ C8 Objects + Callbacks/objects/ejercicios/16.js
@@ -2,6 +2,13 @@ function verificarPassword(objetoUsuario, password) {
   // Verifica si la propiedad "password" del "objetoUsuario" coincide con el parámetro "password".
   // En ese caso, retorna true. Caso contrario, false.
   // Tu código:
+  if (objetoUsuario.password === password) {
+    console.log(`The password ${password} is correct`);
+    return true; 
+  } else {
+    console.log(`The password ${password} is invalid`);
+    return false;
+  }
 }
-
+verificarPassword({name: 'Brey',password:'mikeisCat'},'mikeiscat'); 
 module.exports = verificarPassword;
