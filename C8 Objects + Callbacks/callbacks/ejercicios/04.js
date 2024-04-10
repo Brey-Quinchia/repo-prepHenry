de7f7224f779df1e78e74e71d13c7389bba0c65e
Brey-Quinchia <brehinerqr27@gmail.com>
2024-04-09 20:26:10 -0500

    C8 Callbaks 1-4 ready

--- C8 Objects + Callbacks/callbacks/ejercicios/04.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/04.js
@@ -4,6 +4,14 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // NOTA: no debes retornar nada.
    // Tu código:
+   sumNum = 0
+   for (const num of arrayOfNumbers) {
+      sumNum = num + sumNum;
+   }
+   cb(sumNum);
 }
-
+sumarArray([1,2,3,4,5],function(rst){
+   console.log(`result of sum array values is ${rst}`);
+   return rst;
+})
 module.exports = sumarArray;
